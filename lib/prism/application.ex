11cf7e55747ca6d2883fd37f06566e1c090e94ba
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-06-02 22:03:20 +0200

    Add base code

--- lib/prism/application.ex
+++ lib/prism/application.ex
@@ -1,19 +1,13 @@
 defmodule Prism.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: Prism.Worker.start_link(arg)
-      # {Prism.Worker, arg},
+      {Plug.Adapters.Cowboy, scheme: :http, plug: Prism.Router, options: [port: 4001]},
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Prism.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- test/prism_test.exs
+++ test/prism_test.exs
@@ -1,8 +1,23 @@
 defmodule PrismTest do
-  use ExUnit.Case
-  doctest Prism
+  use ExUnit.Case, async: false
+  use Plug.Test
+
+  @opts Prism.Router.init([])
 
   test "greets the world" do
     assert Prism.hello() == :world
+  end
+
+  test "returns hello world" do
+    # Create a test connection
+    conn = conn(:get, "/hello")
+
+    # Invoke the plug
+    conn = Prism.Router.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "world"
   end
 end
